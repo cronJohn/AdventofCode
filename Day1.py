2765bae2d30bc7387c095a15a0d905b2175a0556
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-12-01 23:57:29 -0600

    Part 2 solved

--- Day1.py
+++ Day1.py
@@ -2,6 +2,13 @@ currentElf = 0
 calorieArray = []
 currentCounter = 0
 
+def top3(arr):
+    sortedarray = sorted(arr,reverse=True)
+    top1 = sortedarray[0]
+    top2 = sortedarray[1]
+    top3 = sortedarray[2]
+    return (top1 + top2 + top3)
+
 # Open puzzle input file
 with open('input.txt','r') as inputObj:
         line = inputObj.readline()
@@ -19,5 +26,9 @@ with open('input.txt','r') as inputObj:
 
         maxCalories = max(calorieArray)
         print(f'Max calories: {maxCalories}')
+
+        top3total = top3(calorieArray)
+        print(f'Top 3 total: {top3total}')
+
 
 
