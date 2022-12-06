76d737632e3173bd96f6f43a8f24879121ef6a17
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-12-06 01:49:19 -0600

    Day 5 part 1 wip 3

--- Day5.py
+++ Day5.py
@@ -1,13 +1,43 @@
 craneBoardArr = []
 
+def getTidiedRows(num):
+    tidiedRows = []
+    num = int(num)
+    end = ((num*3)+(num-1))-2 # Includes letter spaces(which is 3 wide) + separator spaces(which is 1 wide)
+
+    for row in craneBoardArr:
+        start = 0
+        finished = False
+        temp = []
+        while not finished:
+            if row[start:start+3].strip() == "": # Must add strip which fixes an issue if there are spaces after the last letter
+                temp.append('⨯')
+            else:
+                temp.append(row[start+1:start+2])
+            start += 4
+            if start > end:
+                finished = True 
+
+        tidiedRows.append(temp)
+
+    return tidiedRows 
+
+def getNumberOfCols(string):
+    temp = string.split('   ')
+    return max(temp)
+
+def getColumn(array, index):
+    return [row[index] for row in array]
+
+numberOfCols = 0
 with open('day5-input.txt','r') as inputObj:
         line = inputObj.readline()
-        while line:
+        while line: # Looping until number row at the bottom
             line = str(line.strip())
             print(line)
             
             if any(chr.isdigit() for chr in line):
-                print('stop')
+                numberOfCols = getNumberOfCols(line)
                 break
 
             else:
@@ -15,25 +45,15 @@ with open('day5-input.txt','r') as inputObj:
 
             line = inputObj.readline()
 
-tidiedRows = []
-start = 0
-finished = False
-print(f'crane board: {craneBoardArr}')
-for row in craneBoardArr:
-    while not finished:
-        print(f'starting deal: {row[start:start+3]}')
-        if row[start:start+3] == "   ":
-            tidiedRows.append('⨯')
-        else:
-            tidiedRows.append(row[start+1:start+2])
+        tidiedRows = getTidiedRows(numberOfCols)
+        craneColumns = []
 
-        start += 4
+        for i in range(0, len(tidiedRows)):
+            craneColumns.append(getColumn(tidiedRows, i)) # Convert columns to rows
 
-        if start > 35:
-            finished = True 
+        for i in range(0, len(craneColumns)):
+            craneColumns[i] = [x for x in craneColumns[i] if x != "⨯"] # Remove placeholder ⨯
 
-    finished = False
-    start = 0
-    print('--------------------------------')
+        for col in craneColumns:
+            print(col)
 
-print(f'new array: {tidiedRows}')
