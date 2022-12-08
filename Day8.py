2516c3a6b6d671e9796338bc6328b40ed33b042a
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-12-08 13:59:21 -0600

    Day 8 part 2 solved

--- Day8.py
+++ Day8.py
@@ -37,6 +37,60 @@ def checkBounds(x,y):
     else:
         return 0
 
+def calcScenicScore(x,y):
+    total = 1
+    temp = 0
+
+    # Check row mid to left
+    for x_it in range(x, x_start-2, -1):
+        if x != x_it: 
+            if (arr[y][0][x_it] < arr[y][0][x]):
+                temp += 1
+            else:
+                temp += 1
+                break
+
+    total = temp
+    temp = 0
+
+    # Check row mid to right
+    for x_it in range(x, x_end+1):
+        if x != x_it: 
+            if (arr[y][0][x_it] < arr[y][0][x]):
+                temp += 1
+            else:
+                temp += 1
+                break
+
+    total *= temp
+    temp = 0
+
+    # Check col mid to top
+    for y_it in range(y, y_start-2, -1):
+        if y != y_it: 
+            if (arr[y_it][0][x] < arr[y][0][x]):
+                temp += 1
+            else:
+                temp += 1
+                break
+
+    total *= temp
+    temp = 0
+
+
+    # Check col mid to bottom
+    for y_it in range(y, y_end+1):
+        if y != y_it: 
+            if (arr[y_it][0][x] < arr[y][0][x]):
+                temp += 1
+            else:
+                temp += 1
+                break
+    total *= temp
+
+    return total
+
+
 with open('day8-input.txt','r') as inputObj:
         line = inputObj.readline().strip()
         while line:
@@ -48,10 +102,13 @@ x_end = len(arr[0][0]) - 1
 y_start = 1
 y_end = len(arr) - 1
 perimeter_trees = (2*(x_end+1+y_end+1))-4
+deal = []
 
 for row in range(y_start, y_end):
     for col in range(x_start, x_end):
-        total += checkBounds(col, row)
+        #total += checkBounds(col, row)
+        deal.append(calcScenicScore(col,row))
 
-print(f'total {total + perimeter_trees}')
+print(max(deal))
+#print(f'total {total + perimeter_trees}')
 
