fa7517a2db9f8fbe2f90f0167594d375625f5f8d
John Niemiec <51012201+cronJohn@users.noreply.github.com>
2022-12-10 13:03:20 -0600

    Day 9 part 2 solved

--- Day9.py
+++ Day9.py
@@ -8,13 +8,19 @@ moves = {
         "R": [1, 0]
 }
 
+knots = [[0,0] for _ in range(10)]
+
 previousLocations = set()
 
 def inRange():
     return abs(headLocation[0] - tailLocation[0]) <= 1 and abs(headLocation[1] - tailLocation[1]) <= 1
 
+def inRangeArb(x1, y1, x2, y2):
+    return abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1
+
 def addPreviousLocation():
-    previousLocations.add((tailLocation[0], tailLocation[1]))
+    #previousLocations.add((tailLocation[0], tailLocation[1]))
+    previousLocations.add(tuple(knots[-1]))
 
 def parse(string):
     temp = string.split(" ")
@@ -26,20 +32,36 @@ def parse(string):
     
     for _ in range(amount):
         move(dx,dy)
+        #addPreviousLocation()
         addPreviousLocation()
 
 def move(dx, dy):
-    global headLocation, tailLocation
+    global headLocation, tailLocation, knots
 
-    headLocation[0] += dx
-    headLocation[1] += dy
+    # Update first knot in knots
+    knots[0][0] += dx
+    knots[0][1] += dy
 
-    if not inRange():
-        sign_x = 0 if headLocation[0] == tailLocation[0] else (headLocation[0] - tailLocation[0]) / abs(headLocation[0] - tailLocation[0])
-        sign_y = 0 if headLocation[1] == tailLocation[1] else (headLocation[1] - tailLocation[1]) / abs(headLocation[1] - tailLocation[1])
+    #headLocation[0] += dx
+    #headLocation[1] += dy
 
-        tailLocation[0] += sign_x
-        tailLocation[1] += sign_y
+    for i in range(1, 10):
+        hx, hy = knots[i - 1]
+        tx, ty = knots[i]
+
+        if not inRangeArb(hx, hy, tx, ty):
+            sign_x = 0 if hx == tx else (hx - tx) / abs(hx - tx)
+            #sign_x = 0 if headLocation[0] == tailLocation[0] else (headLocation[0] - tailLocation[0]) / abs(headLocation[0] - tailLocation[0])
+            sign_y = 0 if hy == ty else (hy - ty) / abs(hy - ty)
+            #sign_y = 0 if headLocation[1] == tailLocation[1] else (headLocation[1] - tailLocation[1]) / abs(headLocation[1] - tailLocation[1])
+
+            tx += sign_x
+            ty += sign_y
+
+            #tailLocation[0] += sign_x
+            #tailLocation[1] += sign_y
+
+        knots[i] = [tx, ty]
 
 with open('day9-input.txt','r') as inputObj:
         line = inputObj.readline().strip()
